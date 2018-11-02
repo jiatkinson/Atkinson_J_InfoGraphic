9a3cbabb84347f7b26791da772ff588fa5f57991
Jacob Atkinson <jiatkinson@Jacobs-MBP-2.lan>
2018-11-02 15:44:36 -0400

    PHP finished. I am happy, I hope you have a good weekend Jacob! -Jacob

--- js/main.js
+++ js/main.js
@@ -12,7 +12,9 @@
 
     c.start();
 
-const graph = document.querySelector('.section3');
+const graph = document.querySelector('.section3'),
+continentAry = document.querySelectorAll('.continent');
+      
 
 var waypoint = new Waypoint({
     element: document.querySelector('.section3'),
@@ -24,29 +26,62 @@ var waypoint = new Waypoint({
     offset: 600
   });
 
+
+  function fetchData() {
+    console.log(this);
+    fetch(`./includes/connect.php?continentID=${this.dataset.num}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        parseCountryData(data);
+    })
+    .catch(function(error) {
+        console.error(error);
+    });
+}
+
+function parseCountryData(data) {
+  console.log(data);
+  const countryAry = document.querySelectorAll('.countryName'),
+      northAmerica = document.querySelectorAll('.sort_na'),
+      southAmerica = document.querySelectorAll('.sort_sa'),
+      europe = document.querySelectorAll('.sort_europe'),
+      asia = document.querySelectorAll('.sort_asia'),
+      africa = document.querySelectorAll('.sort_africa'),
+      australia = document.querySelectorAll('.sort_australia');
+    // destructure the database info and grab just what we need
+    // const { continent_id, continent_name, country_name } = data
+    // take the database data and put it on the page
+
+    countryAry.forEach(country => country.classList.remove('red'));
+
+  data.forEach(item => {
+
+    if(item.continent_id == 1){// north america
+      northAmerica.forEach(na => na.classList.add('red'));
+    }else if(item.continent_id == 2){//europe
+      europe.forEach(eu => eu.classList.add('red'));
+    } else if (item.continent_id == 3){//africa
+      africa.forEach(af => af.classList.add('red'));
+    } else if (item.continent_id == 4){//asia
+      asia.forEach(as => as.classList.add('red'));
+    } else if (item.continent_id == 5){//south america
+      southAmerica.forEach(sa => sa.classList.add('red'));
+    } else if (item.continent_id == 6){//australia
+      australia.forEach(au => au.classList.add('red'));
+    } else {
+      console.log('this is not a real continent');
+    }
+    });
+
+    
+    
+    
+};
+
+
+
+continentAry.forEach(cont => cont.addEventListener('click', fetchData));
+
+
  })();
-
-
-//  (() => {
-
-//   function fetchData() {
-//       fetch(`./includes/connect.php?countryName=${this.id}`)
-//       .then(res => res.json())
-//       .then(data => {
-//           console.log(data);
-//           parseCountryData(data[0]);
-//       })
-//       .catch(function(error) {
-//           console.error(error);
-//       });
-//   }
-
-//   function parseCountryData(country) {
-//       // destructure the database info and grab just what we need
-//       const { countryName } = country
-//       // take the database data and put it on the page
-//       document.querySelector(".countryName").textContent = countryName;
-//   };
-
-//   // fetchData();
-// })();
